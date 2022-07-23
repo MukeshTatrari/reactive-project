d543dacb29ea1d1e08a24b83df7f6e01b7ea5f8a
MukeshTatrari <mukesh.tatrari@gmail.com>
2022-07-23 16:58:21 +0530

    Added Parallism in reactive streams

--- src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersService.java
+++ src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersService.java
@@ -1,6 +1,8 @@
 package com.learnreactiveprogramming.service;
 
 
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
 
@@ -15,5 +17,50 @@ public class FluxAndMonoSchedulersService {
         delay(1000);
         return name.toUpperCase();
     }
+
+    public Flux<String> explore_WithoutPublisherOn() {
+        var namesFlux = getNamedFlux(Flux.fromIterable(namesList)).log();
+        var namesFlux1 = getNamedFlux(Flux.fromIterable(namesList1)).log();
+
+        return namesFlux.mergeWith(namesFlux1);
+    }
+
+    public Flux<String> explore_PublisherOn() {
+        var namesFlux = getNamedFlux(Flux.fromIterable(namesList).publishOn(Schedulers.parallel())).log();
+        var namesFlux1 = getNamedFlux(Flux.fromIterable(namesList1).publishOn(Schedulers.parallel())).log();
+
+        return namesFlux.mergeWith(namesFlux1);
+    }
+
+    public Flux<String> explore_BoundedElastic() {
+        var namesFlux = getNamedFlux(Flux.fromIterable(namesList).publishOn(Schedulers.boundedElastic())).log();
+        var namesFlux1 = getNamedFlux(Flux.fromIterable(namesList1).publishOn(Schedulers.boundedElastic())).log();
+
+        return namesFlux.mergeWith(namesFlux1);
+    }
+
+    public Flux<String> explore_SubscribeOn() {
+        var namesFlux = getNamedFlux(Flux.fromIterable(namesList)).subscribeOn(Schedulers.boundedElastic()).log();
+        var namesFlux1 = getNamedFlux(Flux.fromIterable(namesList1)).subscribeOn(Schedulers.boundedElastic()).log();
+
+        return namesFlux.mergeWith(namesFlux1);
+    }
+
+    /**
+     *
+     * @param namesList
+     * @return
+     *
+     * this code is blocking which could be a 3rd party library code
+     * which u can not change , so the execution of main thread will be blocked.
+     * to unblock the main thread and execute the whole stream in parallel thread
+     * we can use the SubscribeON
+     * so that the whole stream will be nonblocking.
+     */
+
+    private Flux<String> getNamedFlux(Flux<String> namesList) {
+        return namesList.map(this::upperCase);
+    }
+
 
 }
---
+++ src/test/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersServiceTest.java
@@ -0,0 +1,36 @@
+package com.learnreactiveprogramming.service;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FluxAndMonoSchedulersServiceTest {
+
+    FluxAndMonoSchedulersService fluxAndMonoSchedulersService = new FluxAndMonoSchedulersService();
+
+    @Test
+    void explore_WithoutPublisherOn() {
+        final Flux<String> stringFlux = fluxAndMonoSchedulersService.explore_WithoutPublisherOn();
+        StepVerifier.create(stringFlux).expectNextCount(6).verifyComplete();
+    }
+
+    @Test
+    void explore_PublisherOn() {
+        final Flux<String> stringFlux = fluxAndMonoSchedulersService.explore_PublisherOn();
+        StepVerifier.create(stringFlux).expectNextCount(6).verifyComplete();
+    }
+
+    @Test
+    void explore_PublisherOnBoundedElastic() {
+        final Flux<String> stringFlux = fluxAndMonoSchedulersService.explore_BoundedElastic();
+        StepVerifier.create(stringFlux).expectNextCount(6).verifyComplete();
+    }
+
+    @Test
+    void explore_SubscribeOnBoundedElastic() {
+        final Flux<String> stringFlux = fluxAndMonoSchedulersService.explore_SubscribeOn();
+        StepVerifier.create(stringFlux).expectNextCount(6).verifyComplete();
+    }
+}
